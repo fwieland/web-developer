8f0a7632f1092baa4fd1c9de21dd6788b9aa9e84
EasymoneyF <55006262+EasymoneyF@users.noreply.github.com>
2020-05-05 15:59:55 -0600

    header changes, gif page work

--- js/gifsearch.js
+++ js/gifsearch.js
@@ -1,9 +1,11 @@
-var api = "http://api.giphy.com/v1/gifs/search?";
+/*var api = "http://api.giphy.com/v1/gifs/search?";
 var apiKey = "&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ"
 var query = "&q=funny"
 
 
+http://api.giphy.com/v1/gifs/search?q=funny&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ&limit=12
 
+http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
 
 
 
@@ -18,3 +20,45 @@ function gotData(giphy) {
     createImg(giphy.data[0].images.original.url)
 }
 }
+*/
+(function () {
+  function giphySearch(keyword) {
+    // Make a request to giphy API here
+    return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ&limit=15`).then(response => response.json())
+  }
+
+  function appendImage(img) {
+    let $div = $('<div class="img-wrapper"></div>');
+    $('<div class="inner"></div>').append(img).appendTo($div);
+    $('#thumbs').append($div)
+  }
+
+  function showLoader() {
+    $('.loader-wrapper').addClass('shown');
+  }
+
+  function hideLoader() {
+    $('.loader-wrapper').removeClass('shown');
+  }
+
+  (function listenOnFormSubmit() {
+    $('#searchForm').submit((ev) => {
+      ev.preventDefault();
+      let $input = $('#searchInput');
+
+      main($input.val());
+    });
+  })();
+  
+
+  async function main(searchKeyword) {
+    //Write our code here
+    const result = await giphySearch(searchKeyword);
+    $('thumbs').html('');
+    result.data.forEach(data => {
+        let img = new Image();
+        img.src = data.images.original.url;
+        appendImage(img);
+    })
+  }
+})();
