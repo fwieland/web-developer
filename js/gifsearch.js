a9766e46c513da69dae8fc393d0284b464398ab8
EasymoneyF <55006262+EasymoneyF@users.noreply.github.com>
2020-05-07 12:38:16 -0600

    final touches* gif app

--- js/gifsearch.js
+++ js/gifsearch.js
@@ -1,34 +1,11 @@
-/*var api = "http://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ"
-var query = "&q=funny"
-
-
-http://api.giphy.com/v1/gifs/search?q=funny&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ&limit=12
-
-http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5
-
-
-
-function setup() {
-    noCanvas();
-    var url = api + apiKey + query;
-    loadJSON(url, gotData);
-}
-
-function gotData(giphy) {
-    for (var i = 0; i < giphy.data.length; i++){
-    createImg(giphy.data[0].images.original.url)
-}
-}
-*/
 (function () {
   function giphySearch(keyword) {
     // Make a request to giphy API here
-    return fetch(`https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ&limit=15`).then(response => response.json())
+    return fetch(`https://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=voEUX20dXb64f46gtSfo1eF0HTlFL4PJ&limit=16`).then(response => response.json())
   }
 
   function appendImage(img) {
-    let $div = $('<div class="img-wrapper"></div>');
+    let $div = $('<div class="img-wrapper col-auto"></div>');
     $('<div class="inner"></div>').append(img).appendTo($div);
     $('#thumbs').append($div)
   }
@@ -55,3 +32,8 @@ function gotData(giphy) {
     })
   }
 })();
+
+
+/*
+voEUX20dXb64f46gtSfo1eF0HTlFL4PJ
+*/
